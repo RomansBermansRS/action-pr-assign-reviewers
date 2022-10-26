8776c28ceb811d0fd8eddaa35ec475344b382feb
RomansBermansRS <romans@revelstoke.io>
2022-10-26 16:16:00 +0200

    Updates logic

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ import core from '@actions/core'
 import github from '@actions/github'
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet')
-  console.log(`Hello ${nameToGreet}!`)
-  const time = (new Date()).toTimeString()
-  core.setOutput("time", time)
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`)
+  const octokit = github.getOctokit(core.getInput('github-token'))
+  const context = github.context
+  // const payload = JSON.stringify(context.payload, undefined, 2) 
+  console.log('123', Object.keys(context))
+
+  const result = await octokit.rest.pulls.requestReviewers({
+    // owner, // context.owner,
+    // repo, // context.repo,
+    // pull_number, // context.payload.number,
+    ...context,
+    reviewers: core.getInput('reviewers'),
+    team_reviewers: core.getInput('team-reviewers')
+  });
+
 } catch (error) {
   core.setFailed(error.message)
 }
