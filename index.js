49842442ed5a21ddccedaed947c33c0721d50098
RomansBermansRS <73129964+RomansBermansRS@users.noreply.github.com>
2022-10-27 17:42:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ try {
 
   const reviewers = core.getInput('reviewers').split(',')
   const team_reviewers = core.getInput('team-reviewers').split(',')
+  
+  // TODO: for each team_reviewers call https://octokit.github.io/rest.js/v18#teams-list-members-in-org
 
   const result = await octokit.rest.pulls.requestReviewers({
     owner: payload.repository.owner.login,
